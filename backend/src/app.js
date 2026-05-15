a486a09abf260cc98ffc6cf9f379de5608f54665
Piero Jacinto <pjacinto@santabarbarasa.com.ar>
2026-05-15 09:47:26 -0300

    feat: agregar servidor Express con middleware JSON

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+// importamos express que es el framework para poder crear el servidor y manekjar las rutas de la api. 
+const express = require('express')
+// creamos la instancia de la aplicacion express (lo ejecutamos) para poder usar sus distintos metodos y funcionalidades
+const app = express()
+// este es un middkeware global , que le dice a express que todos los requests que nos lleguen van a ser json
+// entonces exppres automaticamente parsea el body de los req y los convierto a objet js para q podamos trabajar
+app.use(express.json())
+
+const routes = require('./routes/index')
+// importamos las rutas de la api,  todas van a etsar bajo el prefijo /api
+app.use('/api', routes)
+// arrancamos el servidor en el puerto 3000, va a quedar escuchando los requests entrantes
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Backend corriendo en puerto ${PORT}`)
+})
