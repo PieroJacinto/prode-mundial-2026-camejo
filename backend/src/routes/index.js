3f90ba63564209177fe0458a73c459588be87abe
PieroJacinto <pierojacinto@gmail.com>
2026-05-17 14:20:27 -0300

    feat: agregar ruta GET /api/usuarios

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { healthCheck } = require('../controllers/healthController')
+const usuariosController  = require('../controllers/usuariosController')
 
 router.get('/health', healthCheck)
+router.get('/usuarios', usuariosController.getAll)
 
 module.exports = router
