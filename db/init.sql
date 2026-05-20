17f8ea8673e51ef59e4ec4dc251649d9cbc7a371
maveralli-ui <maveralli@fi.uba.ar>
2026-05-19 21:09:36 -0300

    feat: agregar tablas finales de equipos y partidos

--- db/init.sql
+++ db/init.sql
@@ -7,3 +7,20 @@ CREATE TABLE IF NOT EXISTS usuarios (
   saldo_virtual  INT           DEFAULT 1000,
   created_at     TIMESTAMP     DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS equipos (
+  id     SERIAL PRIMARY KEY,
+  pais VARCHAR(100) NOT NULL,
+  grupo  VARCHAR(1) NOT NULL -- VALE LA PENA AGREGAR UN ARRAY CON LOS JUGADORES?
+);
+
+CREATE TABLE IF NOT EXISTS partidos (
+  id               SERIAL PRIMARY KEY,
+  equipo_local_id  INT REFERENCES equipos(id),
+  equipo_visitante_id INT REFERENCES equipos(id),
+  fase             VARCHAR(50) NOT NULL CHECK (fase IN ('Grupos', 'Octavos', 'Cuartos', 'Semifinales', 'Final')),
+  goles_local      INT, -- Lo dejamos vacío (NULL) hasta que se juegue
+  goles_visitante  INT, 
+  fecha            TIMESTAMP NOT NULL,
+  finalizado       BOOLEAN DEFAULT FALSE -- arranca en falso (no se jugó)
+);
