3f116525aa619b53ce36de57822ce37968a1fd77
PieroJacinto <pierojacinto@gmail.com>
2026-05-23 15:22:19 -0300

    fix: eliminar bracketController de las rutas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { healthCheck }    = require('../controllers/healthController')
 const usuariosController = require('../controllers/usuariosController')
-const bracketController  = require('../controllers/bracketController')
 
 router.get('/health',   healthCheck)
 router.get('/usuarios', usuariosController.getAll)
-router.get('/bracket',  bracketController.getBracket)
+
 
 module.exports = router
